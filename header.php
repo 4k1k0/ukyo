cb216dd3a14ae42319635aa43ebb9e07031bd413
Akko <dulo.rodrigo@gmail.com>
2017-09-27 20:08:35 -0500

    Añade el <head> del sitio
    Se añade los datos iniciales dentro de la etiqueta <head> y se añade un
    contenedor <header>.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+<title>
+  <?php wp_title('|', true, 'right'); bloginfo('name'); ?>
+</title>
+<?php wp_head(); ?>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+</head>
+
+<body>
+  <header id="masthead">
+
+  </header>
