f422f099d4b89372b2b9d9474dd447e7595013ab
Akko <dulo.rodrigo@gmail.com>
2017-10-08 18:55:29 -0500

    Añade 404
    Se añade el archivo 404.php para mostrar un mensaje de error.

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<main id="mastwrapper">
+  <div class="container">
+    <article class="articulo-individual">
+      <h1 class="error-404">
+        Lo sentimos, parece que el contenido que buscas todavía no existe.
+        <br>
+        :(
+      </h1>
+    </article>
+  </div>
+</main>
+
+<?php get_footer(); ?>
