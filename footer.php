320c778a0a1aa6d67c4f5172d12260d3b914174a
Akko <dulo.rodrigo@gmail.com>
2018-01-27 01:54:24 -0600

    Añade target blank a footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <footer id="mastfooter">
     <div class="container">
-      
+
       <div id="izquierda" class="col-md-4">
         <p>
           Powered by:
@@ -8,38 +8,38 @@
         <nav>
           <ul>
             <li>
-              <a href="https://es.wordpress.org/">Wordpress</a>
+              <a href="https://es.wordpress.org/" target="_blank">Wordpress</a>
             </li>
             <li>
-              <a href="https://getbootstrap.com/">Bootstrap</a>
+              <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>
             </li>
             <li>
-              <a href="http://fontawesome.io/">Font Awesome</a>
+              <a href="http://fontawesome.io/" target="_blank">Font Awesome</a>
             </li>
             <li>
-              <a href="https://www.advancedcustomfields.com/">Advanced Custom Fields</a>
+              <a href="https://www.advancedcustomfields.com/" target="_blank">Advanced Custom Fields</a>
             </li>
             <li>
-              <a href="https://generatewp.com/">GenerateWP</a>
+              <a href="https://generatewp.com/" target="_blank">GenerateWP</a>
             </li>
           </ul>
         </nav>
       </div>
-      
+
       <div id="centro" class="col-md-4">
         <p>
-          Hecho con 
+          Hecho con
           <span id="corazon">
             ♥
           </span>
         </p>
         <p>
-          <a href="https://github.com/4k1k0/ukyo">
+          <a href="https://github.com/4k1k0/ukyo" target="_blank">
             Código fuente
           </a>
         </p>
       </div>
-      
+
       <div id="derecha" class="col-md-4">
         <p>
           Contacto:
@@ -51,18 +51,18 @@
         <nav>
           <ul>
             <li>
-              <a href="https://twitter.com/akkonomi" class="fa fa-twitter"></a>
+              <a href="https://twitter.com/akkonomi" class="fa fa-twitter" target="_blank"></a>
             </li>
             <li>
-              <a href="https://github.com/4k1k0" class="fa fa-github"></a>
+              <a href="https://github.com/4k1k0" class="fa fa-github" target="_blank"></a>
             </li>
             <li>
-              <a href="https://gs.smuglo.li/akko" class="fa fa-linux"></a>
+              <a href="https://gs.smuglo.li/akko" class="fa fa-linux" target="_blank"></a>
             </li>
           </ul>
         </nav>
       </div>
-      
+
     </div>
   </footer>
 
